fff4037c67dca45036709dba796bee48e9a671ad
MrKangs <gykang00@gmail.com>
2020-04-16 03:49:23 -0700

    Remove is almost done, clear is half way there

--- Assignment 2 Amortized Analysis and Python List Reimplementation Application/student_list.py
+++ Assignment 2 Amortized Analysis and Python List Reimplementation Application/student_list.py
@@ -15,15 +15,17 @@ class StudentList:
     def get_capacity(self):
         return self._capacity
     
+
     def append(self, val):
+        # This function will add an item into the list
         if self._size == self._capacity:
             self._upsize()
 
-        # Needs to check if there is room and call _upsize if there isn't
         self._list[self._size] =  val
         self._size = self._size + 1
 
     def _upsize(self):
+        # If the array hits the limits, it will expand the size of the array by double
 
         new_List = np.empty([self._capacity * 2], np.int16)
 
@@ -34,17 +36,47 @@ class StudentList:
         self._capacity = self._capacity * 2
 
 
+    def _removeRandomValue(self):
+        # This function exsits because when you print it you get an random value of the end of the array which will remove it
+        
+        new_List = np.empty([self._size - 1], np.int16)
+
+        for k in range(self._size -1):
+            new_List[k] = self._list[k]
+        
+        self._list = new_List
+
+    def remove(self, val):
+        # This function will remove the element that is equal to val, not the position of it
+        
+        #FIXME: If the capacity is 4 ,8, 16, 32, ..., then it returns an error
+
+        for k in range(self._size):
+            if self._list[k] == val:
+                self._list = np.delete(self._list, k)
+                print(self._list)
+        
+        self._removeRandomValue()
+
+    def clear(self):
+        # This function will clear the list and return an empty array
+        
+        #FIXME: Both Methods are not working...
+
+        #for k in range(self._size):
+         #   self._list = np.delete(self._list, k)
+          #  self._removeRandomValue()
+           # print(self._list)
+
+        self._list = np.empty([self._capacity], np.int16)
+        for k in range (self._capacity):
+            self._removeRandomValue()
+
     '''
     def pop(self):
         # FIXME
     
     def insert(self, index, val):
-        # FIXME
-    
-    def remove(self, val):
-        # FIXME
-    
-    def clear(self):
         # FIXME
     
     def cout(self):
@@ -56,9 +88,14 @@ class StudentList:
     '''
 student_list = StudentList()
 print(student_list)
-for i in range(17):
+for i in range(8):
   student_list.append(i)
   print(student_list)
+
+#student_list.remove(0)
+#print(student_list)
+
+student_list.clear()
 print(student_list)
 
 
