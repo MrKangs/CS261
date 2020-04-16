9425119d1be20851d64456fc8125830a486aa050
MrKangs <gykang00@gmail.com>
2020-04-16 15:55:10 -0700

    Assignment #2 part 1 complete unless the instructor wants something else...

--- Assignment 2 Amortized Analysis and Python List Reimplementation Application/student_list.py
+++ Assignment 2 Amortized Analysis and Python List Reimplementation Application/student_list.py
@@ -1,27 +1,27 @@
 import numpy as np
 
+
 class StudentList:
     def __init__(self):
         self._list = np.empty([4], np.int16)
         self._capacity = 4
         self._size = 0
-    
+
     def __str__(self):
         return str(self._list[:self._size])
-    
+
     def get_list(self):
         return self._list[:self._size]
-    
+
     def get_capacity(self):
         return self._capacity
-    
 
     def append(self, val):
         # This function will add an item into the list
         if self._size == self._capacity:
             self._upsize()
 
-        self._list[self._size] =  val
+        self._list[self._size] = val
         self._size = self._size + 1
 
     def _upsize(self):
@@ -31,20 +31,19 @@ class StudentList:
 
         for k in range(self._size):
             new_List[k] = self._list[k]
-        
+
         self._list = new_List
         self._capacity = self._capacity * 2
 
-
     def _removeRandomValue(self):
         # This function exsits because when you print it you get an random value of the end of the array which will remove it
-        
+
         new_List = np.empty([self._size - 1], np.int16)
 
-        for k in range(self._size -1):
+        for k in range(self._size - 1):
             new_List[k] = self._list[k]
 
-        self._size = self._size - 1 
+        self._size = self._size - 1
         self._list = new_List
 
     def remove(self, val):
@@ -55,11 +54,9 @@ class StudentList:
             if self._list[k] == val:
                 self._list = np.delete(self._list, k)
                 i += 1
-        
+
         for j in range(i):
             self._removeRandomValue()
-        
-       
 
     def clear(self):
         # This function will clear the list and return an empty array: basically resetting the array 
@@ -71,7 +68,7 @@ class StudentList:
         # This function will count how many element is in the list
         return self._size
 
-    def get(self,index):
+    def get(self, index):
         # This funcion will give the element value in that index position
         return self._list[index]
 
@@ -87,15 +84,16 @@ class StudentList:
         new_List = np.empty([self._size + 1], np.int16)
         for k in range(self._size + 1):
             new_List[k] = self._list[k]
-        new_List = np.insert(new_List,index,val)
-        self._size = self._size + 1 
-        self._list = new_List   
+        new_List = np.insert(new_List, index, val)
+        self._size = self._size + 1
+        self._list = new_List
+
 
 student_list = StudentList()
 print(student_list)
-for i in range(9):
-  student_list.append(i)
-  print(student_list)
+for i in range(900):
+    student_list.append(i)
+    print(student_list)
 
 student_list.append(0)
 student_list.append(1)
@@ -109,9 +107,9 @@ print(student_list)
 student_list.clear()
 print(student_list)
 
-for i in range(10):
-  student_list.append(i)
-  print(student_list)
+for i in range(10000):
+    student_list.append(i)
+    print(student_list)
 
 student_list.pop()
 print(student_list)
@@ -119,6 +117,5 @@ print(student_list)
 student_list.pop()
 print(student_list)
 
-student_list.insert(4,8)
+student_list.insert(4, 8)
 print(student_list)
-
