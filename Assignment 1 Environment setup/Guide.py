a28f6aab92bcd3f1dca45f0293100462a3d1ca5a
MrKangs <gykang00@gmail.com>
2020-04-18 16:57:14 -0700

    Guide line for Set up Python

---
+++ Assignment 1 Environment setup/Guide.py
@@ -0,0 +1,20 @@
+'''
+To do all of the learning parts, you need to setup download Python that the versionis higher than 3.7.
+
+Also, you need to download numpy for package usages. 
+
+You can download Python in this site: https://www.python.org/
+
+Once you donwload python, then search for cmd and enter this: pip install numpy
+
+If the cmd gives you to enter something else, please do that first then reenter the following statement above. 
+
+For text editor: I use VSCODE from Microsoft. You can download from this site: https://code.visualstudio.com/
+
+Once you install VSCODE, I would recommend to download Code Runner and Python Extensions that can provide a lot of support. 
+
+Once you did all of the following, then System_test.py if you install correctly. If there is an issue, then google it!
+
+Ignore the hello_gradescope.py and output.txt
+
+'''
