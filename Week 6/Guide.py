cb78d1c8411dbfeebb68ae899cde86629951c5ba
MrKangs <gykang00@gmail.com>
2020-05-04 03:02:52 -0700

    Week 6 is complete

---
+++ Week 6/Guide.py
@@ -0,0 +1,38 @@
+'''
+# Trees #
+
+1. Look at Tree Overview.png (In Trees folder)
+2. Look at Definition of Trees.png (In Trees folder)
+
+# Binary Trees #
+
+3. Look at Introduction to Binary Trees.png (In Binary Trees folder)
+4. Look at Full Binary Tree.png (In Types of Binary Trees folder)
+5. Look at Perfect Binary Tree.png (In Types of Binary Trees folder)
+6. Look at Complete Binary Tree.png (In Types of Binary Trees folder)
+7. Look at Binary Search Tree.png (In Binary Trees folder)
+
+# Traversals #
+
+8. Look at Traversals.png (In Traversals folder)
+9. Look at Depth First Traversal Part 1 ~ 3.png with the video (In Depth First Traversal folder)
+10. Look at Breadth First Search.png (In Traversals folder)
+
+# Binary Search Tree Operation #
+
+11. Look at Finding Part 1 ~ 2.png (In Finding folder)
+12. Look at Inserting Part 1 ~ 2.png (In Insert folder)
+13. Look at Removing Part 1 ~ 2.png (In Remove folder)
+14. Look at Runtime Complexity of BST Operations.png (In Binary Search Tree Operation folder)
+
+# Homework #
+
+15.
+16.
+17.
+
+
+
+
+
+'''
