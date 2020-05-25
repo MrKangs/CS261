5390e1e55e2befd95508fe6473395f4231490151
MrKangs <gykang00@gmail.com>
2020-05-24 18:47:56 -0700

    Week 7 Complete

---
+++ Week 7/Guide.py
@@ -0,0 +1,21 @@
+'''
+# Balancing with Binary Trees #
+
+1. Look at Limitation of Balancing Trees part 1 ~ 2.png (In Balance with Binary Trees folder)
+2. Look at Balance Part 1 ~ 2.png (In Balance with Binary Trees folder)
+3. Look at this video: https://www.youtube.com/watch?v=LU4fGD-fgJQ&t=165s
+
+# AVL Trees#
+
+4. Look at Rotation.png (In AVL Trees folder)
+5. Look at R_R_Imbalance.gif (In AVL Trees folder)
+6. Look at Determining the Rotations part 1 ~ 3.png (In AVL Trees folder)
+7. Watch the Lecture video (In AVL Trees folder)
+8. Look at Single Rotation part 1 ~ 2.png (In Single Rotation folder)
+9. Look at Double Rotation part 1.png (In Double Rotation folder)
+10. Look at L_R_Imbalance.gif (In Double Rotation folder)
+11. Look at Double Rotation part 2 ~ 4.png (In Double Rotation folder)
+12. Look at Implmentation part 1 ~ 4.png (In Implmentation folder)
+13. Look at Time Complexity.png (In AVL Trees folder)
+
+'''
