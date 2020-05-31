8a559b698d7aa5623179a942ad064d968748423f
MrKangs <gykang00@gmail.com>
2020-05-31 01:07:07 -0700

    Week 9 is complete

---
+++ Week 9/Guide.py
@@ -0,0 +1,30 @@
+'''
+# Maps #
+
+1. Look at Maps.png (In Maps folder)
+
+# Hash Tables #
+
+2. Look at Hash Tables part 1 ~ 2.png (In Hash Tables folder)
+3. Look at Perfect and minimally perfect hash function.png (In Hash Tables folder)
+
+# Heaps Operation #
+
+4. Look at Chaining part 1 ~ 4.png (In Chaining folder from Collision)
+5. Watch the Lecture video (In Chaining folder from Collision)
+
+6. Look at Open Addressing part 1 ~ 4.png (In Open Addressing folder from Collision)
+7. Watch the Lecture video (In Open Addressing folder from Collision)
+
+
+# Homework #
+
+8. Look at hash_map_orginal.py and solve it (In Hash Map from Assignment 5 Hasg Map Implmentation of a Concordance)
+    Solution is hash_map.py
+
+9. Look at word_count_original.py and solve it (In Concordance from Assignment 5 Hasg Map Implmentation of a Concordance)
+    Solution is word_cout.py
+
+10. Look at Questions and solve it (In Written Part from Assignment 5 Hasg Map Implmentation of a Concordance)
+
+'''
